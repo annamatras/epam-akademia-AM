814813cb61d5cdb46393d72ede9d7a41f1bea9da
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:01:05 +0000

    Create test to change directory method

--- src/test/java/CdTest.java
+++ src/test/java/CdTest.java
@@ -1,10 +1,26 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class CdTest{
 
     private final CurrentDirectory currentDirectory = mock(CurrentDirectory.class);
     private final Cd cd = new Cd(currentDirectory);
+
+    @Test
+    public void changeCurrentDirectory() throws Exception {
+        when(currentDirectory.newDir("src")).thenReturn("/home/user");
+
+        String result = cd.run("src");
+
+        assertThat(result).isEqualTo("/home/user");
+        verify(currentDirectory).newDir("src");
+    }
+
 
 
 }
