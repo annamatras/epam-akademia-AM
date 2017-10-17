3482ee0c7383aec6c660386cd693b4fbafcb095c
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:35:15 +0000

    Create method to choose one program

--- src/main/java/ProgramManager.java
+++ src/main/java/ProgramManager.java
@@ -1,2 +1,18 @@
-public class ProgramManager {
+import java.util.List;
+
+class ProgramManager {
+
+    private List<Program> programs;
+
+    ProgramManager(List<Program> programs) {
+        this.programs = programs;
+    }
+
+    Program chooseProgram(String command) {
+        for (Program program : programs) {
+            if (program.name().equals(command))
+                return program;
+        }
+        throw new IllegalArgumentException("Invalid input: " + command);
+    }
 }
