f39ff5c80b04e62b18b35b856832d000b8edba98
Anna Matras <ania.matras@gmail.com>
2017-10-17 12:00:37 +0000

    Create method to run app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         AppFactory factory = new AppFactory();
-        factory.run();
+        factory.getRunner().run();
     }
 }
