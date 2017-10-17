2bbefdcdf823b6c0bda979006d83085c2d8589c7
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:38:18 +0000

    Create test checks if program manager return relevant program

--- src/test/java/ProgramManagerTest.java
+++ src/test/java/ProgramManagerTest.java
@@ -1,5 +1,27 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProgramManagerTest {
 
+    @Test
+    public void returnRelevantProgram() throws Exception {
+        CurrentDirectory currentDirectory = new CurrentDirectory();
+        Dir dir = new Dir(currentDirectory);
+        Cd cd = new Cd(currentDirectory);
+
+        List<Program> programs = new ArrayList<>();
+        programs.add(dir);
+        programs.add(cd);
+        ProgramManager manager = new ProgramManager(programs);
+
+        Program result = manager.chooseProgram("dir");
+        assertThat(result).isEqualTo(dir);
+
+        Program resultCd = manager.chooseProgram("cd");
+        assertThat(resultCd).isEqualTo(cd);
+    }
 }
