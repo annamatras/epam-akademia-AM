314f68253d32eeb85a26860d8501c70cc76c8fe0
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:29:05 +0000

    Create read method to take input from user

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Input {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String read() {
+        scanner.useDelimiter(System.getProperty("line.separator"));
+        return scanner.next();
+    }
 }
