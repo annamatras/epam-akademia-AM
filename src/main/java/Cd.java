149feaba565408177c82b68f8eac98cb44d651ff
Anna Matras <ania.matras@gmail.com>
2017-10-17 10:53:20 +0000

    Create class for cd, dir, exit commands

---
+++ src/main/java/Cd.java
@@ -0,0 +1,21 @@
+public class Cd implements Program {
+
+    private CurrentDirectory currentDirectory;
+
+    Cd(CurrentDirectory currentDirectory) {
+        this.currentDirectory = currentDirectory;
+    }
+
+    public String run(String... args) {
+        if (args.length > 0) {
+            return currentDirectory.newDir(args[0]);
+        } else {
+            return currentDirectory.newDir(".");
+        }
+    }
+
+    @Override
+    public String name() {
+        return "cd";
+    }
+}
---
+++ src/main/java/Dir.java
@@ -0,0 +1,11 @@
+public class Dir implements Program {
+
+
+    public String run(String... args) {
+        return null;
+    }
+
+    public String name() {
+        return null;
+    }
+}
