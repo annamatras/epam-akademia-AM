f3c5917e361643a57048c4a923ff152a6af9c05a
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:17:21 +0000

    Create class Exit with exit and run methods

--- src/main/java/Exit.java
+++ src/main/java/Exit.java
@@ -1,2 +1,14 @@
-public class Exit {
+public class Exit implements Program {
+
+
+    @Override
+    public String run(String... args) {
+        System.exit(0);
+        return "";
+    }
+
+    @Override
+    public String name() {
+        return "exit";
+    }
 }
