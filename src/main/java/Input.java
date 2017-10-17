4755025ab668b8394239374eae54dfda42c55a8f
Anna Matras <ania.matras@gmail.com>
2017-10-17 13:25:00 +0000

    Change info for user

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -5,7 +5,7 @@ class Input {
     private Scanner scanner = new Scanner(System.in);
 
     String read() {
-        System.out.println("Use command: cd, dir or exit are already implemented");
+        System.out.println("Use command: cd, dir and exit are already implemented");
         System.out.println("$>");
         scanner.useDelimiter(System.getProperty("line.separator"));
         return scanner.next();
