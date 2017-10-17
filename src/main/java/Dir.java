71f2dae1009df55f87d7afe8da1a65a2eb55929f
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:14:04 +0000

    Implements run and name methods

--- src/main/java/Dir.java
+++ src/main/java/Dir.java
@@ -1,11 +1,24 @@
 public class Dir implements Program {
 
+    CurrentDirectory currentDirectory;
 
+    public Dir(CurrentDirectory currentDirectory) {
+        this.currentDirectory = currentDirectory;
+    }
+
+    @Override
     public String run(String... args) {
-        return null;
+        String[] list = currentDirectory.list();
+        StringBuilder result = new StringBuilder();
+        String delim = "";
+        for (String s : list) {
+            result.append(delim).append(s);
+            delim = "\n";
+        }
+        return result.toString();
     }
 
     public String name() {
-        return null;
+        return "dir";
     }
 }
