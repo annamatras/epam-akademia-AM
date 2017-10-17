97b2489c19eb7f8e34a47243f41e5d1fc59a6180
Anna Matras <ania.matras@gmail.com>
2017-10-17 12:25:31 +0000

    Create class to run statistics

---
+++ src/main/java/Stats.java
@@ -0,0 +1,12 @@
+public class Stats implements Program {
+
+    @Override
+    public String run(String... args) {
+        return null;
+    }
+
+    @Override
+    public String name() {
+        return null;
+    }
+}
