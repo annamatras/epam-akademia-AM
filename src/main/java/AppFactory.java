e495ed5cb6b23d42ebdf81deb81e7550bf510b66
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:59:50 +0000

    Clean code

--- src/main/java/AppFactory.java
+++ src/main/java/AppFactory.java
@@ -1,6 +1,50 @@
-public class AppFactory {
+import java.util.ArrayList;
+import java.util.List;
 
+class AppFactory {
 
-    public void run() {
+    Runner getRunner() {
+        ProgramManager manager = getProgramManager();
+        Printer printer = getPrinter();
+        Input input = getInput();
+
+        return new Runner(manager, printer, input);
+    }
+
+    private ProgramManager getProgramManager() {
+        CurrentDirectory currentDirectory = getCurrentDirectory();
+        List<Program> programs = new ArrayList<>();
+        programs.add(getDir(currentDirectory));
+        programs.add(getCd(currentDirectory));
+        programs.add(getExit());
+        return new ProgramManager(programs);
+    }
+
+    private Program getDir(CurrentDirectory currentDirectory) {
+        return new Dir(currentDirectory);
+    }
+
+    private Program getCd(CurrentDirectory currentDirectory){
+        return new Cd(currentDirectory);
+    }
+
+    private Program getExit() {
+        return new Exit();
+    }
+
+    private CurrentDirectory getCurrentDirectory() {
+        return new CurrentDirectory();
+    }
+
+    private Printer getPrinter() {
+        return new Printer();
+    }
+
+    void run() {
+
+    }
+
+    public Input getInput() {
+        return new Input();
     }
 }
