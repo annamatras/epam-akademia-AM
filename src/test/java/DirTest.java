9b6d7d35945dee9f90bdadaf9a59c36bde2110c9
Anna Matras <ania.matras@gmail.com>
2017-10-17 11:08:45 +0000

    Create test to check if dir print all folders in directory

--- src/test/java/DirTest.java
+++ src/test/java/DirTest.java
@@ -1,5 +1,21 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DirTest {
 
+    @Test
+    public void printAllFilesInCurrentDirectory() throws Exception {
+
+        CurrentDirectory currentDirectory = mock(CurrentDirectory.class);
+        String[] files = {".", "src", "main"};
+        when(currentDirectory.list()).thenReturn(files);
+        Dir dir = new Dir(currentDirectory);
+
+        String result = dir.run();
+
+        assertThat(result).isEqualTo(".\nsrc\nmain");
+    }
 }
